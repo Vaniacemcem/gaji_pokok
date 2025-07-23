ebb5a533be40054d4f6dc6d04e3900c0b453b8ec
Vaniacemcem <4301180345.vania@gmail.com>
2025-07-23 14:51:58 +0700

    Update gapok.py

--- gapok.py
+++ gapok.py
@@ -23,7 +23,8 @@ golongan_pegawai = ["Pilih Golongan",
                  "Golongan IVb",
                  "Golongan IVc",
                  "Golongan IVd",
-                 "Golongan IVe"]
+                 "Golongan IVe",
+                 "Golongan I"]
 masa_kerja = ["Pilih Masa Kerja", 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
                  
 
