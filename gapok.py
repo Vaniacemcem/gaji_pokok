5f79101ce383142e95153a02b2a9df1a8f02684e
Vaniacemcem <4301180345.vania@gmail.com>
2025-07-23 10:26:45 +0700

    Update gapok.py

--- gapok.py
+++ gapok.py
@@ -14,6 +14,7 @@ masa_kerja = ["Pilih Masa Kerja", 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
 golongan_pegawai = st.selectbox(''':blue[Masukan Golongan]''', golongan_gendut)
 masa_kerjaa = st.selectbox(''':blue[Masukan Masa Kerja (Dalam Tahun)]''',masa_kerja)
 
+# Golongan Ia
 if golongan_pegawai == "Golongan Ia" and 0 <= masa_kerjaa <= 1:
   st.warning ("Gaji Pokoknya adalah 1.685.700")
 
