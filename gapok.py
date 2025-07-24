aa9556dee95c1b7235ee8dbc664a5f4f643d08b0
Vaniacemcem <4301180345.vania@gmail.com>
2025-07-24 14:08:15 +0700

    Update gapok.py

--- gapok.py
+++ gapok.py
@@ -5,8 +5,8 @@ st.set_page_config(
   page_icon="💸",
 )
 
-st.info("""
-# Aplikasi Cek Gaji Pokok ASN
+st.success("""
+# Tools Cek Gaji Pokok ASN
 """)
 st.warning("Ini adalah aplikasi untuk mengecek Gaji Pokok berdasarkan Masa Kerja dan Golongan")
 
@@ -49,9 +49,9 @@ golongan_pegawai = ["Pilih Golongan",
 masa_kerja = ["Pilih Masa Kerja", 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
                  
 
-tipe_pegawaii = st.selectbox(''':blue[Masukkan Tipe Pegawai]''', tipe_pegawai)
-golongan_pegawaii = st.selectbox(''':blue[Masukkan Golongan]''', golongan_pegawai)
-masa_kerjaa = st.selectbox(''':blue[Masukkan Masa Kerja (Dalam Tahun)]''',masa_kerja)
+tipe_pegawaii = st.selectbox('''Masukkan Tipe Pegawai''', tipe_pegawai)
+golongan_pegawaii = st.selectbox('''Masukkan Golongan''', golongan_pegawai)
+masa_kerjaa = st.selectbox('''Masukkan Masa Kerja (Dalam Tahun)''',masa_kerja)
 
 # Golongan Ia
 if tipe_pegawaii == "Pegawai Negeri Sipil (PNS)" and golongan_pegawaii == "Golongan Ia" and 0 <= masa_kerjaa <= 1:
